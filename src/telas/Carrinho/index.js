da6b49c1fe155aec932e4e313f398e1906371d5a
aleprans <alepranskunas@hotmail.com>
2022-04-23 14:42:00 -0300

    Final

--- src/telas/Carrinho/index.js
+++ src/telas/Carrinho/index.js
@@ -2,35 +2,42 @@ import React from 'react';
 import { FlatList } from 'react-native';
 
 import Item from './Item';
+import StatusCarrinho from '../../componentes/StatusCarrinho';
 
 const servicos = [
   {
     id: 1,
     nome: 'banho',
-    preco: 79.90,
-    descricao: 'Banho'
+    preco: 79.9,
+    descricao: 'Banho',
+    quantidade: 1,
   },
   {
     id: 2,
     nome: 'Vacina V4',
     preco: 89.9,
-    descricao: 'Uma dose da vacina V4'
+    descricao: 'Uma dose da vacina V4',
+    quantidade: 2
   },
   {
     id: 3,
     nome: 'vacina anti-rabica',
     preco: 99.9,
-    descricao: 'Uma dose da vacina anti-rabica'
+    descricao: 'Uma dose da vacina anti-rabica',
+    quantidade: 1
   }
 ]
 
-export default function Servicos(){
+export default function Carrinho(){
+  const total = servicos.reduce((soma, {preco, quantidade}) => soma + (preco * quantidade), 0)
+ 
   return <>
+      <StatusCarrinho total={total} />
       <FlatList
         data={servicos}
         removeClippedSubviews={false}
         renderItem={({item}) => <Item {...item} />}
         keyExtractor={({id}) => String(id)}
       />
-  </>
+    </>
 }
